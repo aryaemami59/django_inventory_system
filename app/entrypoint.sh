ceba0208bfd56c100a3a13ad35900506fb8d4a99
arya <aryaemami59@yahoo.com>
2022-05-21 01:42:09 -0500

    removed gunicorn

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,8 +2,9 @@
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+python manage.py runserver 0.0.0.0:8000
 
-docker compose exec django_app python manage.py makemigrations --noinput
-docker compose exec django_app python manage.py migrate --noinput
+# docker compose exec django_app python manage.py makemigrations --noinput
+# docker compose exec django_app python manage.py migrate --noinput
 
-gunicorn storefront.wsgi:application --bind 0.0.0.0:8000
+# gunicorn storefront.wsgi:application --bind 0.0.0.0:8000
